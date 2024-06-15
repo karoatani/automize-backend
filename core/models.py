228d34a7f667f475f5d259760d449af5a741be0c
karoatani <karoatani@gmail.com>
2024-06-15 08:46:50 +0100

    Define model schema for debt

--- core/models.py
+++ core/models.py
@@ -7,8 +7,45 @@ class Account(AbstractUser):
     pass
 
 
+class Person(models.Model):
+    
+    first_name = models.CharField(max_length=255)
+    last_name = models.CharField(max_length=255)
+    phone_number = models.CharField(max_length=20)
+     
+class Debt(models.Model):
+    DEBT_TYPE = (
+            ("CR", "Creditor"),
+           ("DR", "Debtor"),
+    )
+    PAYMENT_FREQUENCY = (
+            ("MONTHLY", "Monthly"),
+           ("BI-MONTHLY", "Bi-Monthly"),
+           ("WEEKLY", "Weekly"),
+           ("BI-WEEKLyY", "Bi-Weekly"),
+           ("QUARTERLY", "Quarterly"),
+           ("SEMI-ANNUALLY", "Semi-Annually"),
+           ("ANNUALLY", "Annually"),
+    )
+    PAYMENT_METHOD = (
+            ("BANK TRANSFER", "Bank Transfer"),
+           ("CASH", "Cash"),
+           ("CREDIT CARD", "Credit Card"),
+           ("DEBIT CARD", "Debit Card"),
+           ("CHECK", "Check"),
+           ("OTHERS", "Others"),
+           )
+    type = models.CharField(max_length=255, choices=DEBT_TYPE)
+    person = models.ForeignKey("Person", on_delete=models.CASCADE)
+    amount = models.IntegerField()
+    currency = models.CharField(max_length=4)
+    due_date = models.DateTimeField()
+    interest_rate = models.FloatField()
+    
+    date_created = models.DateTimeField(auto_now_add=True)
+    last_updated = models.DateTimeField(auto_now=True)
 
-
-class Statement(models.Model):
-    user_statement = models.JSONField()
-    user = models.ForeignKey("Account", on_delete=models.CASCADE)
+    payment_frequency = models.CharField(max_length=20, choices=PAYMENT_FREQUENCY) 
+    payment_method = models.CharField(max_length=50, choices=PAYMENT_METHOD)
+    is_paid = models.BooleanField(default=False)
+    is_bad_debt = models.BooleanField(default=False)
